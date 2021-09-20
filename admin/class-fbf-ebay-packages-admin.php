1366a56b69ee26c65dae8528cd2b3111b98cc93a
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2021-09-20 11:57:49 +0100

    add back clean button

--- admin/class-fbf-ebay-packages-admin.php
+++ admin/class-fbf-ebay-packages-admin.php
@@ -892,7 +892,7 @@ class Fbf_Ebay_Packages_Admin {
             </tfoot>
         </table>
         <button role="button" class="button button-primary" id="fbf_ebay_packages_synchronise" style="margin-top: 1em;" type="button">Synchronise with eBay</button>
-        <!--<button role="button" class="button button-primary" id="fbf_ebay_packages_clean" style="margin-top: 1em;" type="button">Clean eBay</button>-->
+        <button role="button" class="button button-primary" id="fbf_ebay_packages_clean" style="margin-top: 1em;" type="button">Clean eBay</button>
         <span class="spinner" style="margin-top: 1.2em;"></span>
         <br class="clear"/>
         <?php
