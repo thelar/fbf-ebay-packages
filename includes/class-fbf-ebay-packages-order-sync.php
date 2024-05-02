3c0c89c422ab73d48b39999f1bf49afc594148e3
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2024-05-02 10:04:13 +0100

    Pre switch

--- includes/class-fbf-ebay-packages-order-sync.php
+++ includes/class-fbf-ebay-packages-order-sync.php
@@ -54,11 +54,97 @@ class Fbf_Ebay_Packages_Order_Sync extends Fbf_Ebay_Packages_Admin
             require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-fbf-ebay-packages-api-auth.php';
             $auth = new Fbf_Ebay_Packages_Api_Auth();
             $token = $auth->get_valid_token();
-            $orders = $this->api('https://api.ebay.com/sell/fulfillment/v1/order?filter=orderfulfillmentstatus:%7BNOT_STARTED%7CIN_PROGRESS%7D', 'GET', ['Authorization: Bearer ' . $token['token'], 'Content-Type:application/json', 'Content-Language:en-GB']);
-            $real = json_decode($orders['response']);
+            if($token['status']==='success') {
+                $orders = $this->api('https://api.ebay.com/sell/fulfillment/v1/order?filter=orderfulfillmentstatus:%7BNOT_STARTED%7CIN_PROGRESS%7D', 'GET', ['Authorization: Bearer ' . $token['token'], 'Content-Type:application/json', 'Content-Language:en-GB']);
+                if($orders['status']==='success'&&($orders['response_code']===200)){
+                    $this->sync_orders(json_decode($orders['response']));
+                }
+            }
             echo 'ebay orders';
             die();
         }
+    }
+
+    private function sync_orders($orders)
+    {
+        if($orders->total){
+            $orders = $orders->orders;
+            foreach($orders as $order){
+                // 1. Is there an order in Woo for this eBay Order
+                $ebay_order_id = $order->orderId;
+                $args = [
+                    'post_type' => 'shop_order',
+                    'posts_per_page' => -1,
+                    'post_status' => 'any',
+                    'meta_key' => '_ebay_order_number',
+                    'meta_value' => $ebay_order_id,
+                    'meta_compare' => '=',
+                    'fields' => 'ids',
+                ];
+                $woo_order_id = get_posts($args);
+
+                if(empty($woo_order_id)){
+                    // Woo Order does not exist for eBay order so need to create it here
+                    $order = $this->create_order($order);
+                }else{
+                    // Woo Order does exist, check status here
+                    $woo_order = wc_get_order($woo_order_id);
+
+                    // If the Woo Order is Complete - update the eBay Order setting the status to completed and updating the order with the Tracking Information
+                }
+            }
+        }
+    }
+
+    private function create_order($order)
+    {
+        // Use the Woo API to create the order and add the meta
+        $woo_order = wc_create_order();
+
+        // Add the eBay order number as meta
+        update_post_meta($woo_order->get_id(), '_ebay_order_number', $order->orderId);
+
+        // Contact details
+        $names = explode(' ', $order->buyer->buyerRegistrationAddress->fullName);
+        $lastname = array_pop($names);
+        $firstname = implode(' ', $names);
+        $address = array(
+            'first_name' => $firstname,
+            'last_name'  => $lastname,
+            'company'    => $order->buyer->buyerRegistrationAddress->companyName,
+            'email'      => $order->buyer->buyerRegistrationAddress->email,
+            'phone'      => substr($order->buyer->buyerRegistrationAddress->primaryPhone->phoneNumber,0,1)==='0' ? $order->buyer->buyerRegistrationAddress->primaryPhone->phoneNumber : '0' . $order->buyer->buyerRegistrationAddress->primaryPhone->phoneNumber,
+            'address_1'  => $order->buyer->buyerRegistrationAddress->contactAddress->addressLine1,
+            'address_2'  => $order->buyer->buyerRegistrationAddress->contactAddress->addressLine2,
+            'city'       => $order->buyer->buyerRegistrationAddress->contactAddress->city,
+            'state'      => $order->buyer->buyerRegistrationAddress->contactAddress->stateOrProvince,
+            'postcode'   => $order->buyer->buyerRegistrationAddress->contactAddress->postalCode,
+            'country'    => $order->buyer->buyerRegistrationAddress->contactAddress->countryCode,
+        );
+        $woo_order->set_address( $address, 'billing' );
+        $woo_order->set_address( $address, 'shipping' );
+
+        // Taxes
+        $calculate_taxes_for = array(
+            'country'  => $address['country'],
+            'state'    => $address['state'],
+            'postcode' => $address['postcode'],
+            'city'     => $address['city'],
+        );
+
+        // Add the items
+        foreach($order->lineItems as $lineItem){
+            $qty = $lineItem->quantity;
+            $sku_a = explode('.', $lineItem->sku);
+            $sku = $sku_a[array_key_last($sku_a)];
+            $id = wc_get_product_id_by_sku($sku);
+            $item_id = $woo_order->add_product(wc_get_product($id), $qty);
+            $line_item  = $woo_order->get_item( $item_id, false);
+            $line_item->calculate_taxes($calculate_taxes_for);
+            $line_item->save();
+        }
+        $woo_order->calculate_totals();
+        $woo_order->save();
     }
 
     private function api($url, $method, $headers, $body=null)
